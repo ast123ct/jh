422654276668d73b1b6e3d81ae2ca1ecaa102fed
user1 <user1@DESKTOP-BIBKLL9>
2018-05-17 16:14:24 +0900

    test

---
+++ travels/src/notice_board/action/AddAction.java
@@ -0,0 +1,12 @@
+package notice_board.action;
+
+public class AddAction implements Action {
+
+	
+	@Override
+	public ActionForward execute(HttpServletResponse request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
