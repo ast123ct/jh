3c35fd23f04feb9678f1b5fc98f4507ef59ebbbb
한정호 <@DESKTOP-MIT9JGH>
2018-05-17 09:24:09 +0900

    second

--- travels/src/bus/action/controll.java
+++ travels/src/bus/action/controll.java
@@ -27,6 +27,7 @@ public class controll extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println();
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
